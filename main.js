e7181ac2063122c3afb266b410f6cc599d04d6ad
curiousdave1472 <davidshanlay@gmail.com>
2026-05-06 19:53:50 -0700

    main.js
    Prompt the user for name and respond with welcome message.

--- main.js
+++ main.js
@@ -8,3 +8,7 @@
 
 // var firstName = prompt('What is your name?');
 // alert('Welcome ' + firstName + '!');
+prompt ("What is your name");
+var firstname =prompt('What is your name')
+alert ('Welcome ' + 'David' + '!');
+/* This will prompt the user to entet their name and respond with welcome screen.*/
